7ea9c02d82c9441ffa3aab6f565a5fdb0f40c29a
Hatim <hatim.slimani.19@gmail.com>
2024-09-20 17:45:47 +0100

    spliting code into multiple functions : addToCart(), updateCartQuantity(), addedText()

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -64,43 +64,50 @@ productsGrid.innerHTML = productsHTML;
 
 let lastTimeoutId = 0;
 
-document.querySelectorAll('.js-add-to-cart-button')
-    .forEach((button) => {
-        button.addEventListener('click', () => {
-            const productId = button.dataset.productId;
+function addToCart(productId){
+    const matchingItem = cart.find((item) => item.productId === productId);
 
-            const matchingItem = cart.find((item) => item.productId === productId);
+    const quantity = +document.querySelector(`.js-quantity-selector-${productId}`).value;
 
-            const quantity = +document.querySelector(`.js-quantity-selector-${productId}`).value;
-            console.log(quantity);
+    if(matchingItem){
+        matchingItem.quantity += quantity;
+    } else{
+        cart.push({
+            productId: productId,
+            quantity: quantity
+        })
+    }
+}
 
-            if(matchingItem){
-                matchingItem.quantity += quantity;
-            } else{
-                cart.push({
-                    productId: productId,
-                    quantity: quantity
-                })
-            }
+function updateCartQuantity(){
+    let totalCartQuantity = 0;
+    cart.forEach((item) => {
+        totalCartQuantity += item.quantity;
+    })
 
-            let totalCartQuantity = 0;
-            cart.forEach((item) => {
-                totalCartQuantity += item.quantity;
-            })
+    document.querySelector('.js-cart-quantity').innerHTML = totalCartQuantity;
+}
 
-            document.querySelector('.js-cart-quantity').innerHTML = totalCartQuantity;
-            console.log(cart);
-
-            //Added text
-            const added = document.querySelector(`.js-added-${productId}`);
+function addedText(productId){
+    const added = document.querySelector(`.js-added-${productId}`);
             added.classList.add("opacity");
             
             if(lastTimeoutId)   clearTimeout(lastTimeoutId);
             lastTimeoutId = setTimeout(() => {
                 added.classList.remove("opacity");
             }, 2000);
-            console.log(lastTimeoutId);
+}
 
-            
+document.querySelectorAll('.js-add-to-cart-button')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+
+            addToCart(productId);
+
+            updateCartQuantity()
+
+            addedText(productId);
+
         })
     })
