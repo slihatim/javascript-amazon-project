434a4aca37f6cd0fd7b30241a887a9f7e3263108
Hatim <hatim.slimani.19@gmail.com>
2024-09-22 15:43:37 +0100

    generating the cart with javascript

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,8 @@
-export const cart = [];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 11
+},
+{
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    quantity: 5
+}];
